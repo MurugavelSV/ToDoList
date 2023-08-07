26277f28ffa3979ee8e3f9fb5226956f4ac4f49e
Murugavel V <muruvel19.10@gmail.com>
2023-08-07 20:08:55 +0530

    create basic layout of home.ejs and link with home.css

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const port = 8000;
 
 const app = express();
 app.use('/', require('./routes/index'));
+app.use(express.static('./assets'));
 
 app.set('view engine','ejs');
 app.set('views', './views');
