a1f44671a19d42b5ae7b2d7cc4846e07685a7271
Murugavel V <muruvel19.10@gmail.com>
2023-08-07 19:43:19 +0530

    create directory file structure

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const port = 8000;
+
+const app = express();
+app.use('/', require('./routes/index'));
+
+app.set('view engine','ejs');
+app.set('views', './views');
+
+app.listen(port, (err) => {
+    if(err){
+        console.log(`Error in starting the server : ${err.message}`);
+    }
+
+    console.log(`Server is up and running at port : ${port}`);
+})
