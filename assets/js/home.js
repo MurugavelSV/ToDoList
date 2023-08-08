701081948ea953c215011eef3b4776330c0c00dc
Murugavel V <muruvel19.10@gmail.com>
2023-08-08 22:04:50 +0530

    add js file in home.ejs and update the styles

---
+++ assets/js/home.js
@@ -0,0 +1,29 @@
+let categoryContainer = document.getElementsByClassName('category');
+let paraText = document.querySelectorAll('.category > p');
+
+for(let i = 0;i < paraText.length;i++){
+    let bgColor = "";
+
+    switch(paraText[i].innerText.toLowerCase()){
+        case "personal":
+            bgColor = "#40487c";
+            break;
+
+        case "work":
+            bgColor = "#9c00af";
+            break;
+
+        case "school":
+            bgColor = "#f2a700";
+            break;
+
+        case "cleaning":
+            bgColor = "#df5e5e";
+            break;
+
+        default:
+            bgColor = "#64aa76";
+            break;
+    }
+    categoryContainer[i].style.backgroundColor = bgColor;
+}
