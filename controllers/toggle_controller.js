1d2499d8683d25008449560cacbd521805029ea5
Murugavel V <muruvel19.10@gmail.com>
2023-08-09 15:38:38 +0530

    complete styles when task is toggled

--- controllers/toggle_controller.js
+++ controllers/toggle_controller.js
@@ -3,13 +3,10 @@ const Tasks = require('../models/toDoTasks');
 module.exports.toggleTask = (req, res) => {
     (async () => {
         let taskId = req.query.id;
-        console.log(req.query.status);
         let currentStatus = req.query.status;
-        console.log(currentStatus);
         await Tasks.updateOne(
             {_id: taskId}, {completed: currentStatus == 'true'? false: true}
-        ).then((data) => {
-            console.log(data);
+        ).then(() => {
             return res.redirect('/');
         }).catch((err) => {
             console.log(`Error: ${err.message}`);
